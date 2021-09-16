81ae6cbfbd5a02c00c67998d00f43057e58740d7
Ano97 <63305763+Anoosha-ui@users.noreply.github.com>
2021-09-16 18:32:15 +0200

    Add files via upload

---
+++ TURN_180.ino
@@ -0,0 +1,12 @@
+void Turn_180()
+{Forward
+delay(1000);
+   OCR2A=100; //pin10
+ OCR2B=100; //pin9  
+  
+digitalWrite(8,HIGH);
+digitalWrite(11,LOW);
+digitalWrite(12,HIGH);
+digitalWrite(7,LOW);
+delay(1000);
+  }
